31fb506b17c0f8c51d2155820c6e8e89952bdf35
Crowdbotics <engineering@crowdbotics.com>
2023-07-08 23:10:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "proud-resonance-dev-99084",
+  url: "https://proud-resonance-dev-99084.botics.co",
+  api: "https://proud-resonance-dev-99084.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
